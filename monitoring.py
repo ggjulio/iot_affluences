b7fb501abf5990beab70bb69d20f7d5603574fe2
ggjulio <juligonz@student.42.fr>
2022-07-06 11:44:46 +0200

    .

--- monitoring.py
+++ monitoring.py
@@ -4,18 +4,19 @@ import psutil
 import socket
 import datetime as dt
 
+CPU_PROBE_INTERVAL=1
 DEVICE_PATH="/dev/sda1"
 
-def get_report(device_disk_path: str = DEVICE_PATH):
+def get_report(cpu_probe_interval: int, device_disk_path: str):
 	date=dt.datetime.now().astimezone().isoformat(timespec='seconds')
 	hostname=socket.gethostname()
-	cpu=psutil.cpu_percent(2)
+	cpu=psutil.cpu_percent(1)
 	ram=psutil.virtual_memory().percent
 	disk=psutil.disk_usage('/dev/sda1').percent
 	return (f"[{date}] {hostname} - {cpu};{ram};{disk}")
 
 def main():
-	print(get_report('/dev/sda2'))
+	print(get_report(CPU_PROBE_INTERVAL, DEVICE_PATH))
 
 if __name__ == "__main__":
 	main()
